dd6b09c77f83a019558c48d4962172cf409cf854
Mark <macvik17@mail.ru>
2020-04-05 20:09:43 +0300

    feat: add solution to task2

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -1,7 +1,25 @@
 #import "Combinator.h"
 
+double factorial(int n) {
+    if (n == 0) return 1;
+    else return n*factorial(n-1);
+};
+
+double binomialCoefficient(int n, int k) {
+    double temp = n;
+    for (int i = 1; i < k; i++) temp *= (n-i);
+    return temp/factorial(k);
+}
+
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    int N = [array[0] intValue], n = [array[1] intValue], k = 1;
+    double m = n;
+    while ((N != m || m < N) && k < n) {
+        k++;
+        m = binomialCoefficient(n, k);
+    }
+    if (N == m) return @(k);
+    else return nil;
 }
 @end
